110ff5a4df78d376cdd682692dfa73e245fdb2f2
KhoaVoUS <khoavr23@gmail.com>
2023-07-18 17:36:09 +0700

    Need fix the last 4 sort algo

--- SortAlg.h
+++ SortAlg.h
@@ -1,30 +1,30 @@
 #pragma once
 
-void selectionSort(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void selectionSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void insertionSort(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void insertionSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void bubbleSort(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void bubbleSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void heapSort(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void heapSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void countingSort(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void countingSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void radixSort(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void radixSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void qSortCount(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void qSortCount(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void mergeSortCount(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void mergeSortCount(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void quickSort(int* array, int size, int &count_assign, int& count_compare);
+void quickSort(int* array, int size, long long&count_assign, long long& count_compare);
 
-void mergeSort(int* array, int size, int &count_assign, int& count_compare);
+void mergeSort(int* array, int size, long long&count_assign, long long& count_compare);
 
-void radixSort(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void radixSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void shellSort(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void shellSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void shakerSort(int* array, int size, int &count_assign, int& count_compare, float& Time);
+void shakerSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void SortChoosing(int* array, int size, int sortType, int &count_assign, int &count_compare, float &Time);
+void SortChoosing(int* array, int size, int sortType, long long& count_assign, long long& count_compare, float &Time);
 
--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@
 
 using namespace std;
 
-
-int main()
+void print()
 {
+        freopen("output.txt", "w", stdout);
     for (int dataType = 0; dataType < 4 ; dataType++)
     {
         /* Data Size Examine the selected sorting algorithms on data of the following sizes: 10,000, 30,000, 50,000, 100,000, 300,000, and 500,000 elements.*/
         for (int dataSize = 10000; dataSize <= 50000; dataSize += 20000)
         {
             int* array = new int[dataSize];
-            int count_assign = 0;
-            int count_compare = 0;
+            long long count_assign = 0;
+            long long count_compare = 0;
             float Time = 0;
             for (int sortType = 1; sortType <= 10; sortType++)
             {
@@ -88,19 +88,96 @@ int main()
             delete[] array;
         }
         for (int dataSize = 100000; dataSize <= 500000; dataSize += 200000)
-                {
+            {
             int* array = new int[dataSize];
-            int count_assign = 0;
-            int count_compare = 0;
+            long long count_assign = 0;
+            long long count_compare = 0;
             float Time = 0;
             for (int sortType = 1; sortType <= 10; sortType++)
             {
                 GenerateData(array, dataSize, dataType);
                 SortChoosing(array, dataSize, sortType, count_assign, count_compare, Time);
-               
+                               cout<<"Data type : ";
+                switch (dataType)
+                {
+                case 0:
+                    cout<<"Random"<<endl;
+                    break;
+                case 1:
+                    cout<<"Sorted"<<endl;
+                    break;
+                case 2:
+                    cout<<"Reverse"<<endl;
+                    break;
+                case 3:
+                    cout<<"Nearly Sorted"<<endl;
+                    break;
+                }
+                cout<<"Data size : "<<dataSize<<endl;
+                cout<<"Sort type : ";
+                switch (sortType)
+                {
+                case 1:
+                    cout<<"Selection sort"<<endl;
+                    break;
+                case 2:
+                    cout<<"Insertion sort"<<endl;
+                    break;
+                case 3:
+                    cout<<"Bubble sort"<<endl;
+                    break;
+                case 4:
+                    cout<<"Heap sort"<<endl;
+                    break;
+                case 5:
+                    cout<<"Counting sort"<<endl;
+                    break;
+                case 6:
+                    cout<<"Radix sort"<<endl;
+                    break;
+                case 7:
+                    cout<<"Quick sort"<<endl;
+                    break;
+                case 8:
+                    cout<<"Merge sort"<<endl;
+                    break;
+                case 9:
+                    cout<<"Shell sort"<<endl;
+                    break;
+                case 10:
+                    cout<<"Shaker sort"<<endl;
+                    break;
+                }
+                cout<<"Number of assignment : "<<count_assign<<endl;
+                cout<<"Number of comparison : "<<count_compare<<endl;
+                cout<<"Time : "<<Time<<endl;
+                cout<<"---------------------------------"<<endl;
             }
             delete[] array;
         }
     }
+}
+int main()
+{
+    int array[10];
+    GenerateData(array, 10, 0);
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+    long long count_assign = 0;
+    long long count_compare = 0;
+    float Time = 0;
+    SortChoosing(array, 10, 9, count_assign, count_compare, Time);
 
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"Number of assignment : "<<count_assign<<endl;
+    cout<<"Number of comparison : "<<count_compare<<endl;
+    cout<<"Time : "<<Time<<endl;
+    cout<<"---------------------------------"<<endl;
 }
