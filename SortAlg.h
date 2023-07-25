630a192871a295db22d1acf396f85b5d3153a783
xiao-honsu <122676915+xiao-honsu@users.noreply.github.com>
2023-07-25 10:10:55 +0700

    Update SortAlg.h

--- SortAlg.h
+++ SortAlg.h
@@ -1,54 +1,51 @@
 #pragma once
 
-void selectionSortTime(int* array, int size, float& Time);
+void selectionSortTime(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void selectionSort(int* array, int size, long long&count_assign, long long& count_compare);
+void selectionSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void insertionSortTime(int* array, int size, float& Time);
+void insertionSortTime(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void insertionSort(int* array, int size, long long&count_assign, long long& count_compare);
+void insertionSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void bubbleSortTime(int* array, int size, float& Time);
+void bubbleSortTime(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void bubbleSort(int* array, int size, long long&count_assign, long long& count_compare);
+void bubbleSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void heapSortTime(int* array, int size, float& Time);
+void heapSortTime(int* array, int size, long long & count_assign, long long& count_compare, float& Time);
 
-void heapSort(int* array, int size, long long&count_assign, long long& count_compare);
+void heapSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void countingSort(int* array, int size, long long&count_assign, long long& count_compare);
+void countingSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void countingSortTime(int* array, int size, float&Time);
+void radixSortTime(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void radixSortTime(int* array, int size, float& Time);
+void radixSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void radixSort(int* array, int size, long long&count_assign, long long& count_compare);
+void quickSortTime(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void quickSortTime(int* array, int size, float& Time);
+void mergeSortCountTime(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void mergeSortTime(int* array, int size, float& Time);
-
-void mergeSort(int* array, int size, long long&count_assign, long long& count_compare);
+void mergeSortTime(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
 void quickSort(int* array, int size, long long&count_assign, long long& count_compare);
 
-void radixSortTime(int* array, int size, float& Time);
+void mergeSort(int* array, int size, long long&count_assign, long long& count_compare);
 
-void radixSort(int* array, int size, long long&count_assign, long long& count_compare);
+void radixSortTime(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void shellSortTime(int* array, int size, float& Time);
+void radixSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void shellSort(int* array, int size, long long&count_assign, long long& count_compare);
+void shellSortTime(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void shakerSortTime(int* array, int size, float& Time);
+void shellSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void shakerSort(int* array, int size, long long&count_assign, long long& count_compare);
+void shakerSortTime(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void flashSortTime(int* array, int size, float& Time);
+void shakerSort(int* array, int size, long long&count_assign, long long& count_compare, float& Time);
 
-void flashSort(int* array, int size, long long& count_assign, long long& count_compare);
+void flashSortTime(int* array, int size, long long& count_assign, long long& count_compare, float& Time);
 
-void SortChoosing(int* array, int size, int sortType, long long& count_assign, long long& count_compare);
+void flashSort(int* array, int size, long long& count_assign, long long& count_compare, float& Time);
 
-void SortChoosingTime(int* array, int size, int sortType, float &Time);
-
+void SortChoosing(int* array, int size, int sortType, long long& count_assign, long long& count_compare, float &Time);
